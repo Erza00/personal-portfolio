cf323f0fa4f1ea81313b4462f5a787e269eedb73
Erza00 <132174450+Erza00@users.noreply.github.com>
2025-03-05 05:37:58 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,8 @@
+/*===========my links===============*/
+document.querySelectorAll('#my-link').addEventListener("touchstart",function(){
+  window.location.href="https://erza00.github.io/interior-web/","https://erza00.github.io/adventigo-web/","https://erza00.github.io/grill-web/";
+})
+
 /*=============== Show Menu ================= */
 const navMenu = document.getElementById('nav-menu');
 navToggle = document.getElementById('nav-toggle');
